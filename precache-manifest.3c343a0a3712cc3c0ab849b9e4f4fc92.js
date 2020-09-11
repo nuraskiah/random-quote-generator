979722194e7d354a23149922706af4eb619b6f22
Nur Askiah <askiahnur1@gmail.com>
2020-09-12 00:00:21 +0800

    Updates

---
+++ precache-manifest.3c343a0a3712cc3c0ab849b9e4f4fc92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1006e4ddcb1f3a04e73e3583a811ea1",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "b4793559e9bc326d09ba",
+    "url": "/random-quote-generator/static/css/main.efbaa465.chunk.css"
+  },
+  {
+    "revision": "525e4e8c6589afee802d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.e930acd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4793559e9bc326d09ba",
+    "url": "/random-quote-generator/static/js/main.9503c746.chunk.js"
+  },
+  {
+    "revision": "efbd8013faa4b47d47b4",
+    "url": "/random-quote-generator/static/js/runtime-main.04b377e6.js"
+  }
+]);
